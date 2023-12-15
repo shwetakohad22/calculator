bc5f521d7083892ba20aabff6881fa63b1da5deb
shwetakohad22 <shwetakohad22@gmail.com>
2023-12-15 19:03:28 +0530

    calculator

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ function calculate() {
   let result = eval(expression);
   document.getElementById("result").value = result;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("keypress", handleKeyPress);
+});
+
+function handleKeyPress(event) {
+  const keyPressed = event.key;
+  if (isNaN(keyPressed)) {
+    alert("Only numbers are allowed!");
+  }
+}
